cf84af1dea6a7eb94d3f80e672a451abe5210d0a
AbrorIT <sultanovabror578@gmail.com>
2023-01-13 18:21:49 +0600

    search

--- apps/products/views.py
+++ apps/products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from apps.products.serializers import ProductSerializer,ProductCreateSerializer,ProductUpdateSerializer
 from apps.products.models import Product
+from rest_framework.generics import ListAPIView
+from rest_framework import filters
 
 # Create your views here.
 
@@ -21,3 +23,9 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
+
+class ProductSearchAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ('product', 'category')
